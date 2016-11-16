fbdd79751e6b38eedf776600fd5a5e5930a2965f
Sean Dow Miller <seandowmiller@gmail.com>
2016-11-16 11:32:01 -0800

    I dont know if my overlap works

--- flight.js
+++ flight.js
@@ -3,7 +3,12 @@
 
 var xValue = Number(document.getElementById("Bill").getAttribute("x"))
 var yValue = Number(document.getElementById("Bill").getAttribute("y"))
-
+var xPrincipia = Number(document.getElementById("Issac").getAttribute("x"))
+var yPrincipia = Number(document.getElementById("Issac").getAttribute("y"))
+var universalGravWidth = Number(document.getElementById("Issac").getAttribute("width"))
+var universalGravHeight = Number(document.getElementById("Issac").getAttribute("height"))
+var vunerability = false;
+var abdomenHurt = false;
 
 document.addEventListener("keydown", function(e) {
  if (e.keyCode == 37) {
@@ -24,4 +29,18 @@ document.addEventListener("keydown", function(e) {
   document.getElementById("Bill").setAttribute("y", yValue + 10)
      yValue = yValue + 10;
  }
+ if (e.keyCode == 32) {
+   if(vunerability = true){
+     abdomenHurt = true;
+   }
+ }
+
+ if (xValue > xPrincipia & xValue < xPrincipia + universalGravWidth & yValue > yPrincipia & yValue < yPrincipia + universalGravHeight){
+   vunerability = true;
+ }
+
+ if(abdomenHurt = true){
+   document.getElementById("Issac").setAttribute("opacity", 0)
+ }
+
 })

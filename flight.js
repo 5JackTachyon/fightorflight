87d927c4e2b26880f529b952a56088168daeb416
Sean Dow Miller <seandowmiller@gmail.com>
2016-11-17 11:30:26 -0800

    I got Bill to make Issac dissappear

--- flight.js
+++ flight.js
@@ -3,12 +3,13 @@
 
 var xValue = Number(document.getElementById("Bill").getAttribute("x"))
 var yValue = Number(document.getElementById("Bill").getAttribute("y"))
-var xPrincipia = Number(document.getElementById("Issac").getAttribute("x"))
-var yPrincipia = Number(document.getElementById("Issac").getAttribute("y"))
-var universalGravWidth = Number(document.getElementById("Issac").getAttribute("width"))
-var universalGravHeight = Number(document.getElementById("Issac").getAttribute("height"))
-var vunerability = false;
-var abdomenHurt = false;
+//var xPrincipia = Number(document.getElementById("Issac").getAttribute("x"))
+//var yPrincipia = Number(document.getElementById("Issac").getAttribute("y"))
+//var universalGravWidth = Number(document.getElementById("Issac").getAttribute("width"))
+//var universalGravHeight = Number(document.getElementById("Issac").getAttribute("height"))
+//var vunerability = false;
+//var abdomenHurt = false;
+var appleADay = Number(document.getElementById("Issac").getAttribute("opacity"))
 
 document.addEventListener("keydown", function(e) {
  if (e.keyCode == 37) {
@@ -29,18 +30,40 @@ document.addEventListener("keydown", function(e) {
   document.getElementById("Bill").setAttribute("y", yValue + 10)
      yValue = yValue + 10;
  }
- if (e.keyCode == 32) {
-   if(vunerability = true){
-     abdomenHurt = true;
-   }
- }
+//  if (e.keyCode == 32) {
+  // if(vunerability = true){
+    // abdomenHurt = true;
+  // }
+ //}
 
- if (xValue > xPrincipia & xValue < xPrincipia + universalGravWidth & yValue > yPrincipia & yValue < yPrincipia + universalGravHeight){
-   vunerability = true;
- }
+//if (xValue > xPrincipia && xValue < xPrincipia + universalGravWidth && yValue > yPrincipia && yValue < yPrincipia + universalGravHeight){
+  // vunerability = true;
+ //}
 
+ //if(abdomenHurt = true){
+//   document.getElementById("Issac").setAttribute("opacity", 0)
+ //}
+
+})
+
+document.addEventListener("click", function (e) {
+
+  var xNye = Number(document.getElementById("Bill").getAttribute("x"))
+  var yNye = Number(document.getElementById("Bill").getAttribute("y"))
+  var xPrincipia = Number(document.getElementById("Issac").getAttribute("x"))
+  var yPrincipia = Number(document.getElementById("Issac").getAttribute("y"))
+  var universalGravWidth = Number(document.getElementById("Issac").getAttribute("width"))
+  var universalGravHeight = Number(document.getElementById("Issac").getAttribute("height"))
+  var abdomenHurt = false;
+
+
+if (xValue > xPrincipia && xValue < xPrincipia + universalGravWidth && yValue > yPrincipia && yValue < yPrincipia + universalGravHeight){
+ abdomenHurt = true;
  if(abdomenHurt = true){
-   document.getElementById("Issac").setAttribute("opacity", 0)
+  document.getElementById("Issac").setAttribute("opacity", 0)
  }
+}
+
+
 
 })

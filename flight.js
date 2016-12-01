95c334f951a4e13f3bcc841f0c3ce74b962d2286
Sean Dow Miller <seandowmiller@gmail.com>
2016-12-01 11:30:07 -0800

    working on enemies

--- flight.js
+++ flight.js
@@ -3,6 +3,8 @@
 
 var xValue = Number(document.getElementById("Bill").getAttribute("x"))
 var yValue = Number(document.getElementById("Bill").getAttribute("y"))
+var xProton = Number(document.getElementById("Bill").getAttribute("x"))
+var yElectron = Number(document.getElementById("Bill").getAttribute("y"))
 //var xPrincipia = Number(document.getElementById("Issac").getAttribute("x"))
 //var yPrincipia = Number(document.getElementById("Issac").getAttribute("y"))
 //var universalGravWidth = Number(document.getElementById("Issac").getAttribute("width"))
@@ -12,7 +14,7 @@ var yValue = Number(document.getElementById("Bill").getAttribute("y"))
 var appleADay = Number(document.getElementById("Issac").getAttribute("opacity"))
 var appleADay1 = Number(document.getElementById("Issac1").getAttribute("opacity"))
 var hitCount = 0;
-var health = 15;
+var health = 20;
 var timeStart = Date.now();
 var issacSpeed = 3;
 var issacSpeed1 = 3;
@@ -83,27 +85,32 @@ document.addEventListener("click", function (e) {
   var yPrincipia = Number(document.getElementById("Issac").getAttribute("y"))
   var universalGravWidth = Number(document.getElementById("Issac").getAttribute("width"))
   var universalGravHeight = Number(document.getElementById("Issac").getAttribute("height"))
+  var xWidth = Number(document.getElementById("Issac").getAttribute("width"))
+  var yWidth = Number(document.getElementById("Issac").getAttribute("height"))
   var xPrincipia1 = Number(document.getElementById("Issac1").getAttribute("x"))
   var yPrincipia1 = Number(document.getElementById("Issac1").getAttribute("y"))
   var universalGravWidth1 = Number(document.getElementById("Issac1").getAttribute("width"))
   var universalGravHeight1 = Number(document.getElementById("Issac1").getAttribute("height"))
   var abdomenHurt = false;
   var abdomenHurt1 = false;
+  var radioActive = false;
   function randomNumberGenerator(min,max)
   {
       return Math.floor(Math.random()*(max-min+1)+min);
   }
 
   var value1 = randomNumberGenerator(1,600);
+  var value2 = randomNumberGenerator(2,600);
 
 if (xValue > xPrincipia && xValue < xPrincipia + universalGravWidth && yValue > yPrincipia && yValue < yPrincipia + universalGravHeight){
  abdomenHurt = true;
  if(abdomenHurt == true){
-   issacSpeed = issacSpeed - 0.02;
+   issacSpeed = issacSpeed - 0.5;
    document.getElementById("Prism").setAttribute("dur", issacSpeed);
+   document.getElementById("Apple").setAttribute("dur", issacSpeed1);
   document.getElementById("Issac").setAttribute("x", value1)
   hitCount = hitCount + 1;
-  health = health + 5;
+  health = health + 10;
   document.getElementById("HEALTH").textContent = "Health: " + health;
   document.getElementById("shouldersOfGiants").textContent = "Score: " + hitCount;
  }
@@ -111,16 +118,25 @@ if (xValue > xPrincipia && xValue < xPrincipia + universalGravWidth && yValue > 
 if (xValue > xPrincipia1 && xValue < xPrincipia1 + universalGravWidth1 && yValue > yPrincipia1 && yValue < yPrincipia1 + universalGravHeight1){
  abdomenHurt1 = true;
  if(abdomenHurt1 == true){
-   issacSpeed1 = issacSpeed1 - 0.02;
+   issacSpeed1 = issacSpeed1 - 0.5;
    document.getElementById("Prism1").setAttribute("dur", issacSpeed1);
-  document.getElementById("Issac1").setAttribute("x", value1)
+   document.getElementById("Apple1").setAttribute("dur", issacSpeed1);
+  document.getElementById("Issac1").setAttribute("x", value2)
   hitCount = hitCount + 1;
-  health = health + 5;
+  health = health + 10;
   document.getElementById("HEALTH").textContent = "Health: " + health;
   document.getElementById("shouldersOfGiants").textContent = "Score: " + hitCount;
  }
 }
-if(hitCount == 20){
+if (xProton > xValue && xPhoton < xValue + xWidth && yElectron > yValue && yElectron < yValue + yWidth){
+ radioActive = true;
+ if(radioActive == true){
+  health = health - 10;
+  document.getElementById("HEALTH").textContent = "Health: " + health;
+  document.getElementById("shouldersOfGiants").textContent = "Score: " + hitCount;
+ }
+}
+if(hitCount > 19){
   document.getElementById("gameOverText").textContent = "You Won";
   document.getElementById("screen").pauseAnimations()
   var timeStop = Date.now();
